a15d5e4cbe21e09e6842b3ce504aff5fcae2f522
Saul Lara <hl.saul.01@gmail.com>
2025-06-29 01:07:49 -0600

    🔐 FEAT: Setup SSH agent and add deploy key for remote access

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 ssh -o StrictHostKeyChecking=no azureuser@20.121.67.185 << EOF
 
+    # Start the SSH agent
+    eval "$(ssh-agent -s)"
+
+    # Add SSH deploy key
+    ssh-add ~/.ssh/deploy-key
+
     cd /home/azureuser/swa-actions-course
 
     git pull --rebase origin master
