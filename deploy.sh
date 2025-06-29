f6368778c55406b1bbe64953857bcf9b6317c71a
Saul Lara <hl.saul.01@gmail.com>
2025-06-29 00:18:23 -0600

    🐛 FIX: Update incorrect branch name in script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ ssh -o StrictHostKeyChecking=no azureuser@20.121.67.185 << EOF
 
     cd /home/azureuser/swa-actions-course
 
-    git pull --rebase origin main
+    git pull --rebase origin master
 
 EOF
