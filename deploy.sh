1a52e21ce5e7f018731d3e9e46821f472b4742e3
Saul Lara <hl.saul.01@gmail.com>
2025-06-30 04:56:42 -0600

    👌 IMPROVE: Add branch-based deployment conditions to script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
-ssh -o StrictHostKeyChecking=no azureuser@20.121.67.185 <<EOF
+BRANCH=$1
 
-    cd /home/azureuser/swa-actions-course
+if [ "$BRANCH" == "master" ]; then
+    DEPLOY_PATH="/home/azureuser/swa-actions-course"
+else
+    DEPLOY_PATH="/home/azureuser/staging-server"
+fi
 
-    git pull --rebase origin master
+ssh -o StrictHostKeyChecking=no azureuser@20.121.67.185 << EOF
+
+    cd $DEPLOY_PATH
+
+    git pull --rebase origin $BRANCH
 
 EOF
